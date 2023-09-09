35ad414df15fb9f328daf241db991a660a01dc18
Maksim Kostromin <maksim@beachfront.com>
2023-09-09 01:06:17 -0400

    Fix year bug.

--- app.ts
+++ app.ts
@@ -4,9 +4,9 @@ interface User {
 }
 
 function greet(user: User): string {
-    return `Hello, ${user.name}, as far as I can see you are ${user.age} years old!`;
+    return `Hello ${user.name}! As far as I can see you are ${user.age} years old!`;
 }
 
 console.log(
-    greet({name: 'Maksimko', age: 39})
+    greet({name: 'Maksimko', age: new Date().getFullYear() - 1983})
 )
